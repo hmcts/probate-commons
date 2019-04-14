ed0c48944666ae197ccf48cddc5f9c9a13aea21a
Jake <jake.huggins@solirius.com>
2019-04-14 22:37:45 +0100

    PRO-4890: add abstract ErrorResponse and ErrorType

---
+++ src/main/java/uk/gov/hmcts/reform/probate/model/client/ErrorResponse.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.probate.model.client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
+@JsonSubTypes(value = {})
+public abstract class ErrorResponse {
+
+    @JsonTypeId
+    private ErrorType type;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/probate/model/client/ErrorType.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.probate.model.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static uk.gov.hmcts.reform.probate.model.client.ErrorType.Constants.API_CLIENT_ERROR;
+import static uk.gov.hmcts.reform.probate.model.client.ErrorType.Constants.VALIDATION_ERROR;
+
+@ApiModel(value = "ErrorType", description = "Represents probate error response type")
+@RequiredArgsConstructor
+public enum ErrorType {
+
+    @JsonProperty(VALIDATION_ERROR) VALIDATION(VALIDATION_ERROR),
+    @JsonProperty(API_CLIENT_ERROR) API_CLIENT(API_CLIENT_ERROR);
+
+    @Getter
+    private final String name;
+
+    public static class Constants {
+
+        public static final String VALIDATION_ERROR = "VALIDATION";
+        public static final String API_CLIENT_ERROR = "API_CLIENT";
+
+        private Constants() {
+        }
+    }
+}
