efd6106f026c3a535c1718c934ac48619c9a99bb
ruban72 <ruban72@hotmail.com>
2019-11-05 20:34:05 +0000

    PRO-6212: Strip out pounds signs

---
+++ src/test/java/uk/gov/hmcts/reform/probate/model/jackson/BigDecimalDeserializerTest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.probate.model.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BigDecimalDeserializerTest {
+
+    BigDecimalDeserializer bigDecimalDeserializer = new BigDecimalDeserializer();
+    @Mock
+    JsonParser mockJsonParser;
+    @Mock
+    DeserializationContext mockDeserializarionContext;
+
+    @Test
+    public void shouldDeserializeBigDecimal() throws IOException {
+        Mockito.when(mockJsonParser.getCurrentToken()).thenReturn(JsonToken.VALUE_STRING);
+        Mockito.when(mockJsonParser.getText()).thenReturn("£100,000.00");
+        BigDecimal result = bigDecimalDeserializer.deserialize(mockJsonParser, mockDeserializarionContext);
+
+        assertThat(result, is(new BigDecimal("100000.00")));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenInvalidText() throws IOException {
+        Mockito.when(mockJsonParser.getCurrentToken()).thenReturn(JsonToken.VALUE_STRING);
+        Mockito.when(mockJsonParser.getText()).thenReturn("$r100,000.00");
+        BigDecimal result = bigDecimalDeserializer.deserialize(mockJsonParser, mockDeserializarionContext);
+    }
+}
