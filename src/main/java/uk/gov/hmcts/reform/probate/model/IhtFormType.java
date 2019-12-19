c2393d40160a3e7b03ee7b345d87217c6c348c28
ruban72 <ruban72@hotmail.com>
2019-12-19 16:08:03 +0000

    PRO-6387: change json value of NA on IhtFormType

--- src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormType.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT205_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT207_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT400421_VALUE;
-import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_APPLICABLE;
+import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_APPLICABLE_VALUE;
 
 @RequiredArgsConstructor
 public enum IhtFormType {
@@ -15,7 +15,7 @@ public enum IhtFormType {
     @JsonProperty(IHT205_VALUE) IHT205(IHT205_VALUE),
     @JsonProperty(IHT207_VALUE) IHT207(IHT207_VALUE),
     @JsonProperty(IHT400421_VALUE) IHT400421(IHT400421_VALUE),
-    @JsonProperty(NOT_APPLICABLE) NOTAPPLICABLE(NOT_APPLICABLE);
+    @JsonProperty(NOT_APPLICABLE_VALUE) NOTAPPLICABLE(NOT_APPLICABLE_VALUE);
 
     @Getter
     private final String description;
@@ -37,7 +37,7 @@ public enum IhtFormType {
 
         public static final String IHT400421_VALUE = "IHT400421";
 
-        public static final String NOT_APPLICABLE = "Not applicable";
+        public static final String NOT_APPLICABLE_VALUE = "NA";
 
         private Constants() {
         }
