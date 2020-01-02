9d6b54d2070245192e97b85e12c92a1c12078174
ruban72 <ruban72@hotmail.com>
2020-01-02 10:16:35 +0000

    WLA-22: add bilingual flag to invite data

--- src/main/java/uk/gov/hmcts/reform/probate/model/multiapplicant/Invitation.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/multiapplicant/Invitation.java
@@ -3,10 +3,14 @@ package uk.gov.hmcts.reform.probate.model.multiapplicant;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.probate.model.jackson.OptionYesNoDeserializer;
+import uk.gov.hmcts.reform.probate.model.jackson.OptionYesNoSerializer;
 
 import java.io.Serializable;
 import javax.validation.constraints.Size;
@@ -57,4 +61,7 @@ public class Invitation implements Serializable {
     @JsonProperty("agreed")
     private Boolean agreed;
 
+    @JsonDeserialize(using = OptionYesNoDeserializer.class)
+    @JsonSerialize(using = OptionYesNoSerializer.class)
+    private Boolean bilingual;
 }
