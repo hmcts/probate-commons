046080e5ea0f2b0cd1012a291906d2a450a3da9c
itoulou <touloumbadjiani@gmail.com>
2020-11-27 10:55:26 +0000

    change fields from executor commons to executor applying

--- src/main/java/uk/gov/hmcts/reform/probate/model/forms/pa/Executor.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/forms/pa/Executor.java
@@ -76,12 +76,4 @@ public class Executor {
     @JsonSerialize(using = OptionYesNoSerializer.class)
     private Boolean executorAgreed;
 
-    @JsonDeserialize(using = YesNoDeserializer.class)
-    @JsonSerialize(using = YesNoSerializer.class)
-    private Boolean applyingExecutorEmailChanged;
-
-    @JsonDeserialize(using = YesNoDeserializer.class)
-    @JsonSerialize(using = YesNoSerializer.class)
-    private Boolean applyingExecutorEmailSent;
-
 }
