0954b491d8e53601a5dbe5836c03b1ddce73a612
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-09-15 14:51:44 +0100

    adding probate case with event description

---
+++ src/main/java/uk/gov/hmcts/reform/probate/model/cases/ProbateCaseWithEventDescription.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.probate.model.cases;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class ProbateCaseWithEventDescription {
+
+    private ProbateCaseDetails probateCaseDetails;
+
+    private String eventDescription;
+    
+}
