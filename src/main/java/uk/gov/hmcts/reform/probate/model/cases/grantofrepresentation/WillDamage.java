27e61f252a44a2e1c6a70ab1b8d45fbff4a42d0d
itoulou <touloumbadjiani@gmail.com>
2021-09-23 16:23:58 +0100

    checkstyles

--- src/main/java/uk/gov/hmcts/reform/probate/model/cases/grantofrepresentation/WillDamage.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/cases/grantofrepresentation/WillDamage.java
@@ -1,14 +1,10 @@
 package uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.probate.model.jackson.YesNoDeserializer;
-import uk.gov.hmcts.reform.probate.model.jackson.YesNoSerializer;
 
 import java.util.List;
 
@@ -18,27 +14,6 @@ import java.util.List;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WillDamage {
-//    @JsonDeserialize(using = YesNoDeserializer.class)
-//    @JsonSerialize(using = YesNoSerializer.class)
-//    private Boolean stapleOrPunchHoles;
-//
-//    @JsonDeserialize(using = YesNoDeserializer.class)
-//    @JsonSerialize(using = YesNoSerializer.class)
-//    private Boolean rustMarks;
-//
-//    @JsonDeserialize(using = YesNoDeserializer.class)
-//    @JsonSerialize(using = YesNoSerializer.class)
-//    private Boolean paperClipMarks;
-//
-//    @JsonDeserialize(using = YesNoDeserializer.class)
-//    @JsonSerialize(using = YesNoSerializer.class)
-//    private Boolean tornEdges;
-//
-//    @JsonDeserialize(using = YesNoDeserializer.class)
-//    @JsonSerialize(using = YesNoSerializer.class)
-//    private Boolean waterDamage;
-
     private List<String> willDamageTypesList;
-
     private String otherDamageDescription;
 }
--- src/main/java/uk/gov/hmcts/reform/probate/model/forms/WillDamageTypes.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/forms/WillDamageTypes.java
@@ -1,14 +1,10 @@
 package uk.gov.hmcts.reform.probate.model.forms;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.probate.model.jackson.OptionYesNoDeserializer;
-import uk.gov.hmcts.reform.probate.model.jackson.OptionYesNoSerializer;
 
 import java.util.List;
 
@@ -18,27 +14,6 @@ import java.util.List;
 @Builder
 @ApiModel(value = "WillDamageTypes", description = "Represents will condition selections in ccd")
 public class WillDamageTypes {
-//    @JsonDeserialize(using = OptionYesNoDeserializer.class)
-//    @JsonSerialize(using = OptionYesNoSerializer.class)
-//    private Boolean stapleOrPunchHoles;
-//
-//    @JsonDeserialize(using = OptionYesNoDeserializer.class)
-//    @JsonSerialize(using = OptionYesNoSerializer.class)
-//    private Boolean rustMarks;
-//
-//    @JsonDeserialize(using = OptionYesNoDeserializer.class)
-//    @JsonSerialize(using = OptionYesNoSerializer.class)
-//    private Boolean paperClipMarks;
-//
-//    @JsonDeserialize(using = OptionYesNoDeserializer.class)
-//    @JsonSerialize(using = OptionYesNoSerializer.class)
-//    private Boolean tornEdges;
-//
-//    @JsonDeserialize(using = OptionYesNoDeserializer.class)
-//    @JsonSerialize(using = OptionYesNoSerializer.class)
-//    private Boolean waterDamage;
-
     private List<String> willDamageTypesList;
-
     private String otherDamageDescription;
 }
