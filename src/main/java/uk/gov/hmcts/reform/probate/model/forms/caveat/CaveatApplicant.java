ba544b94a78d7b30703e94e485606e5cca5c7489
DelythJustice <97962563+DelythJustice@users.noreply.github.com>
2022-07-28 14:30:26 +0100

    Update CaveatApplicant.java
    CaveatApplicant is for digital journey, we only need to work bulk scan journey for now

--- src/main/java/uk/gov/hmcts/reform/probate/model/forms/caveat/CaveatApplicant.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/forms/caveat/CaveatApplicant.java
@@ -1,16 +1,12 @@
 package uk.gov.hmcts.reform.probate.model.forms.caveat;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.probate.model.cases.caveat.ProbateFee;
-import uk.gov.hmcts.reform.probate.model.cases.caveat.ProbateFeeNotIncludedReason;
 import uk.gov.hmcts.reform.probate.model.forms.Address;
 import uk.gov.hmcts.reform.probate.model.forms.Applicant;
-import uk.gov.hmcts.reform.probate.model.forms.Language;
 
 import javax.validation.constraints.NotNull;
 
@@ -18,7 +14,6 @@ import javax.validation.constraints.NotNull;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @EqualsAndHashCode(callSuper = false)
 public class CaveatApplicant extends Applicant {
 
@@ -32,21 +27,5 @@ public class CaveatApplicant extends Applicant {
     private String lastName;
 
     private Address address;
-
-    private String caveatorPhoneNumber;
-
-    private ProbateFee probateFee;
-
-    private ProbateFeeNotIncludedReason probateFeeNotIncludedReason;
-
-    private String helpWithFeesReference;
-
-    private String probateFeeNotIncludedExplanation;
-
-    private String probateFeeAccountNumber;
-
-    private String probateFeeAccountReference;
-
-    private Language bilingualCorrespondenceRequested;
 
 }
