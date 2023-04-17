a58fffa905c605c306887c5e8074ce4f7c86cd38
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2023-04-17 12:02:37 +0100

    DTSPB-3449 Fix check style

--- src/main/java/uk/gov/hmcts/reform/probate/model/cases/CaseState.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/cases/CaseState.java
@@ -177,6 +177,7 @@ public enum CaseState {
         public static final String CAVEAT_CLOSED_NAME = "CaveatClosed";
 
         public static final String DORMANT_NAME = "Dormant";
+
 	public static final String BO_POST_GRANT_ISSUED_NAME = "BOPostGrantIssued";
 
         private Constants() {
