d068a940897acc9e52aff619a382898c65dbc96f
FeliTam <feli.tam@justice.gov.uk>
2023-07-21 15:41:18 +0100

    DTSPB-3640 solsWillType

--- src/main/java/uk/gov/hmcts/reform/probate/model/cases/grantofrepresentation/SolicitorWillType.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/cases/grantofrepresentation/SolicitorWillType.java
@@ -5,15 +5,16 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.SolicitorWillType.Constants.GRANT_TYPE_ADMON_VALUE;
-import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.SolicitorWillType.Constants.GRANT_TYPE_PROBATE_VALUE;
 import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.SolicitorWillType.Constants.GRANT_TYPE_INTESTACY_VALUE;
+import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.SolicitorWillType.Constants.GRANT_TYPE_PROBATE_VALUE;
 
 @RequiredArgsConstructor
 public enum SolicitorWillType {
 
     @JsonProperty(GRANT_TYPE_ADMON_VALUE) GRANT_TYPE_ADMON(GRANT_TYPE_ADMON_VALUE),
-    @JsonProperty(GRANT_TYPE_PROBATE_VALUE) GRANT_TYPE_PROBATE(GRANT_TYPE_ADMON_VALUE),
-    @JsonProperty(GRANT_TYPE_INTESTACY_VALUE) GRANT_TYPE_INTESTACY(GRANT_TYPE_INTESTACY_VALUE);
+
+    @JsonProperty(GRANT_TYPE_INTESTACY_VALUE) GRANT_TYPE_INTESTACY(GRANT_TYPE_INTESTACY_VALUE),
+    @JsonProperty(GRANT_TYPE_PROBATE_VALUE) GRANT_TYPE_PROBATE(GRANT_TYPE_PROBATE_VALUE);
 
     @Getter
     private final String name;
@@ -21,8 +22,8 @@ public enum SolicitorWillType {
     public static class Constants {
 
         public static final String GRANT_TYPE_ADMON_VALUE = "WillLeftAnnexed";
-        public static final String GRANT_TYPE_PROBATE_VALUE = "WillLeft";
         public static final String GRANT_TYPE_INTESTACY_VALUE = "NoWill";
+        public static final String GRANT_TYPE_PROBATE_VALUE = "WillLeft";
 
         private Constants() {
         }
