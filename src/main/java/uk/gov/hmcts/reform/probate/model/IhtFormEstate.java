888c337266694d8b9bb04d10b2d207421fc2f699
aramchurn21 <Adesh.Ramchurn@justice.gov.uk>
2023-10-26 10:25:07 +0100

    DTSPB-3705-hmrc-unique-probate-code-probate-commons added optionIHT400 and optionNotRequired field

--- src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormEstate.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormEstate.java
@@ -6,13 +6,16 @@ import lombok.RequiredArgsConstructor;
 
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT207_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT400421_VALUE;
+import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT400_VALUE;
+import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_REQUIRED_VALUE;
 
 
 @RequiredArgsConstructor
 public enum IhtFormEstate {
 
     @JsonProperty(IHT207_VALUE) optionIHT207(IHT207_VALUE),
-    @JsonProperty(IHT400421_VALUE) optionIHT400421(IHT400421_VALUE);
+    @JsonProperty(IHT400421_VALUE) optionIHT400421(IHT400421_VALUE), optionIHT400(IHT400_VALUE),
+    optionNotRequired(NOT_REQUIRED_VALUE) ;
 
     @Getter
     private final String description;
