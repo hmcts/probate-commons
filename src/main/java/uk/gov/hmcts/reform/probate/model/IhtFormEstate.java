7f73892cb2381426a55438e0bfdb65105841b879
DelythJustice <97962563+DelythJustice@users.noreply.github.com>
2023-11-10 12:48:26 +0000

    Update IhtFormEstate.java

--- src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormEstate.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormEstate.java
@@ -14,7 +14,7 @@ import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_REQUIR
 @RequiredArgsConstructor
 public enum IhtFormEstate {
 
-    @JsonProperty(IHT205_VALUE) optionIHT207(IHT205_VALUE),
+    @JsonProperty(IHT205_VALUE) optionIHT205(IHT205_VALUE),
     @JsonProperty(IHT207_VALUE) optionIHT207(IHT207_VALUE),
     @JsonProperty(IHT400421_VALUE) optionIHT400421(IHT400421_VALUE),
     @JsonProperty(IHT400_VALUE) optionIHT400(IHT400_VALUE),
