9c063a5d8621d2c3f6aebc94eeca6cd11410abe7
DelythJustice <delyth.jones11@justice.gov.uk>
2023-11-23 14:36:24 +0000

    DTSPB-3705 1.4.42

--- src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormEstate.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/IhtFormEstate.java
@@ -4,19 +4,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT205_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT207_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT400421_VALUE;
 import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.IHT400_VALUE;
+import static uk.gov.hmcts.reform.probate.model.IhtFormType.Constants.NOT_APPLICABLE_VALUE;
 
 
 @RequiredArgsConstructor
 public enum IhtFormEstate {
 
-    @JsonProperty(IHT205_VALUE) optionIHT205(IHT205_VALUE),
     @JsonProperty(IHT207_VALUE) optionIHT207(IHT207_VALUE),
     @JsonProperty(IHT400421_VALUE) optionIHT400421(IHT400421_VALUE),
-    @JsonProperty(IHT400_VALUE) optionIHT400(IHT400_VALUE);
+    @JsonProperty(IHT400_VALUE) optionIHT400(IHT400_VALUE),
+    @JsonProperty(NOT_APPLICABLE_VALUE) optionNA(NOT_APPLICABLE_VALUE);
 
     @Getter
     private final String description;
