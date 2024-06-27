daa496c6d09781c5232ba9a1a511ef3c57968df7
Marcus Ho <99657446+MarcusHMCTS@users.noreply.github.com>
2024-06-27 11:34:12 +0100

    Revert "Dtspb 4070 deceased will name rems"

--- src/main/java/uk/gov/hmcts/reform/probate/model/forms/pa/PaDeceased.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/forms/pa/PaDeceased.java
@@ -18,14 +18,13 @@ import java.util.Map;
 public class PaDeceased extends ProbateDeceased {
 
     @Builder
-    public PaDeceased(String firstName, String lastName, Boolean nameAsOnTheWill, String aliasFirstNameOnWill,
-                      String aliasLastNameOnWill, Boolean alias, Map<String, AliasOtherNames> otherNames,
+    public PaDeceased(String firstName, String lastName, Boolean alias, Map<String, AliasOtherNames> otherNames,
                       Boolean married, Address address, String postcode, String postcodeAddress, Boolean addressFound,
                       List<Map<String, Object>> addresses, LocalDateTime dateOfBirth, LocalDateTime dateOfDeath,
                       String domicile, Boolean diedEngOrWales, String deathCertificate, Boolean englishForeignDeathCert,
                       Boolean foreignDeathCertTranslation) {
-        super(firstName, lastName, nameAsOnTheWill, aliasFirstNameOnWill, aliasLastNameOnWill, alias, otherNames,
-                married, address, postcode, postcodeAddress, addressFound, addresses, dateOfBirth, dateOfDeath,
-                domicile, diedEngOrWales, deathCertificate, englishForeignDeathCert, foreignDeathCertTranslation);
+        super(firstName, lastName, alias, otherNames, married, address, postcode, postcodeAddress, addressFound,
+                addresses, dateOfBirth, dateOfDeath, domicile, diedEngOrWales, deathCertificate,
+                englishForeignDeathCert, foreignDeathCertTranslation);
     }
 }
