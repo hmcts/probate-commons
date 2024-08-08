3a1b7ba2d3c4221bce782c2d85ee901667199ec4
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2024-08-08 10:28:28 +0100

    fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/probate/model/multiapplicant/ExecutorNotification.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/multiapplicant/ExecutorNotification.java
@@ -3,17 +3,12 @@ package uk.gov.hmcts.reform.probate.model.multiapplicant;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
