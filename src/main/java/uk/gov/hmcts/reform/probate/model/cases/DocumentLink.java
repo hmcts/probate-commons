e4404f6f8c3f6afd31af2ad284db234d317490b8
abbasiTahir <tahir.abbasi@justice.gov.uk>
2024-08-28 10:38:56 +0100

    DTSPB-4115 added the timestamp to DocumentLink.java

--- src/main/java/uk/gov/hmcts/reform/probate/model/cases/DocumentLink.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/cases/DocumentLink.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @AllArgsConstructor
