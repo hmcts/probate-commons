bde935ac4e351c188968dda0b1241afe179eedaa
FeliTam <feli.tam@justice.gov.uk>
2024-10-02 17:26:12 +0100

    DTSPB-4264 update CitizenDocument

--- src/main/java/uk/gov/hmcts/reform/probate/model/forms/CitizenDocument.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/forms/CitizenDocument.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.probate.model.forms;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.probate.model.cases.DocumentLink;
-
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -18,11 +15,4 @@ public class CitizenDocument {
 
     @JsonProperty("DocumentLink")
     private DocumentLink documentLink;
-
-    @JsonProperty("DocumentFileName")
-    private String documentFileName;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @JsonProperty("DocumentDateAdded")
-    private LocalDate documentDateAdded;
 }
