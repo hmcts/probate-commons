a82e5f87498b5502ced1095c85485c407216cadf
FeliTam <feli.tam@justice.gov.uk>
2024-10-14 10:29:38 +0100

    DTSPB-4264 add citizenHubUpload doctype

--- src/main/java/uk/gov/hmcts/reform/probate/model/ProbateDocumentType.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/ProbateDocumentType.java
@@ -51,6 +51,9 @@ public enum ProbateDocumentType {
     @JsonProperty("other")
     OTHER("other"),
 
+    @JsonProperty("citizenHubUpload")
+    CITIZEN_HUB_UPLOAD("citizenHubUpload"),
+
     @JsonProperty("edgeCase")
     EDGE_CASE("edgeCase"),
 
