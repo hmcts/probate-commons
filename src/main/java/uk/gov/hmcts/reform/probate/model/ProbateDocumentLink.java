8e6b60803824f4617ac28a2db7c69c18bbc9e6b9
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-11-05 10:48:56 +0000

    DTSPB-4115: Add serialisation

--- src/main/java/uk/gov/hmcts/reform/probate/model/ProbateDocumentLink.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/ProbateDocumentLink.java
@@ -2,10 +2,16 @@ package uk.gov.hmcts.reform.probate.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
@@ -22,4 +28,10 @@ public class ProbateDocumentLink {
 
     @JsonProperty(value = "document_filename")
     private String documentFilename;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonProperty(value = "upload_timestamp")
+    private LocalDateTime uploadTimestamp;
+
 }
