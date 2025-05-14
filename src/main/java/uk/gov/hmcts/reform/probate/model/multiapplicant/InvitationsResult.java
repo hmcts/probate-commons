6253b5305cd5e556679f0f075eb98a80ae9e58ea
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-05-14 18:42:44 +0100

    DTSPB-4663 Add LastModifiedDateTime to InvitationsResult

--- src/main/java/uk/gov/hmcts/reform/probate/model/multiapplicant/InvitationsResult.java
+++ src/main/java/uk/gov/hmcts/reform/probate/model/multiapplicant/InvitationsResult.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.probate.model.multiapplicant;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -17,5 +19,8 @@ import java.util.List;
 public class InvitationsResult implements Serializable {
 
     private List<Invitation> invitations;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime lastModifiedDateTime;
 
 }
