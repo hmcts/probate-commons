259d4b83ff61848c885f12a63e2bdd5564806be3
FeliTam <feli.tam@justice.gov.uk>
2026-06-24 17:37:26 +0100

    DTSPB-5808 remove pebble and revert related changes in DTSPB-4679

--- src/main/java/uk/gov/hmcts/reform/probate/exception/PdfRenderException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.probate.exception;
-
-public class PdfRenderException extends ProbateRuntimeException {
-    public PdfRenderException(
-            final String message,
-            final Throwable cause) {
-        super(message, cause);
-    }
-}
